374caf0dad2b98f3ae8267a5b3cb96759c68fcc4
Jean-David Therrien <jeandavidt@gmail.com>
2024-06-06 14:13:38 -0400

    Revert "ported dateaubase import from RODTOX project" This should have been committed to RODTOX project, not the template This reverts commit abdeb9592fc56b88b0cb5f129da261687f727fc8.

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+"""This file contains functions used to test the functionality of functions in the project. Name each test with the prefix test_.
+"""
+import pandas as pd
+from src import main
+
+
+def test_square_series():
+    # set up the data
+    test_input = pd.Series([1, 2, 3, 4])
+
+    expected_output = pd.Series([1, 4, 9, 16])
+
+    assert main.square_series(test_input).equals(expected_output)
